94efb20536bafe771f45e08dabec8f487a5b6fd1
Ethan Andersen <ethanandersen@outlook.com>
2017-12-11 11:02:23 -0500

    Fixed tab apocolipse

--- math_land.py
+++ math_land.py
@@ -9,38 +9,26 @@ from math_lib.environment import Environment
 from math_lib.vector import Vector
 
 def run_game():
-	#initialize program, settings and create screen object
+    #initialize program, settings and create screen object
     #comment for first commit sina
-	pygame.init()
-	ml_settings = Settings()
-	screen = pygame.display.set_mode((ml_settings.screen_width, ml_settings.screen_height))
-	pygame.display.set_caption("Math Land")
-
-	#Initialize mathematical environment
-	environment = Environment()
+    pygame.init()
+    ml_settings = Settings()
+    screen = pygame.display.set_mode((ml_settings.screen_width, ml_settings.screen_height))
+    pygame.display.set_caption("Math Land")
+    #Initialize mathematical environment
+    environment = Environment()
     #Environment origin is set to center of screen
-	environment.set_origin(Vector(screen.get_rect().centerx,screen.get_rect().centery))
+    environment.set_origin(Vector(screen.get_rect().centerx,screen.get_rect().centery))
 
-	#Create Cartisian plane object
-	cartesian_plane = Cartesian_Plane(environment)
-
-  
-    
-	#Start main loop
-	while True:
-        
-        	#Check for events
-        	ws_f.check_events()
-		
-		#Update the environment
-		
-		
-
-		#Update any objects on screen
-		cartesian_plane.update(environment)
-
-		#Draw the screen
-		ws_f.update_screen(ml_settings, screen, cartesian_plane)
-
-        
+    #Create Cartisian plane object
+    cartesian_plane = Cartesian_Plane(environment)
+    #Start main loop
+    while True: 
+        #Check for events
+        ws_f.check_events()
+        #Update the environment
+        #Update any objects on screen
+        cartesian_plane.update(environment)
+        #Draw the screen
+        ws_f.update_screen(ml_settings, screen, cartesian_plane)     
 run_game()
